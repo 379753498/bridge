496b97efe25843326c2fb0eff9cc24104647d00f
TestDevWay <379753498@qq.com>
2018-06-11 18:06:38 +0800

    FutureTaskdemo

---
+++ DataTestdev/src/com/zeone/util/FutureTaskCallableFileTest.java
@@ -0,0 +1,72 @@
+package com.zeone.util;
+
+import java.io.File;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+import org.apache.commons.io.FileUtils;
+
+import com.zeone.bean.databaen;
+import com.zeone.lifeline.collector.util.DateUtil;
+import com.zeone.txt.imp.FileFactoryReadimp;
+
+public class FutureTaskCallableFileTest {
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws InterruptedException,
+			ExecutionException {
+
+		FileFactoryReadimp FileFactoryReadimp = new FileFactoryReadimp();
+		// 获取文件列表
+
+		String path = "D://bridgetestdemo";// 定义文件路径
+		ArrayList<String> filelist = FileFactoryReadimp.getfilenames(path);
+
+		ExecutorService ExecutorService = Executors.newFixedThreadPool(3);
+		List<FutureTask<ArrayList<databaen>>> FutureTask = new ArrayList<FutureTask<ArrayList<databaen>>>();
+		for (int i = 0; i < filelist.size(); i++) {
+
+			String string = filelist.get(i).toString();
+			FileCallable FileCallable = new FileCallable(string,
+					FileFactoryReadimp.getfilename(string));
+			FutureTask<ArrayList<databaen>> fa = new FutureTask<ArrayList<databaen>>(
+					FileCallable);
+			ExecutorService.submit(fa);
+			FutureTask.add(fa);
+
+		}
+		ExecutorService.shutdown();
+
+		for (FutureTask<ArrayList<databaen>> future : FutureTask) {
+			System.out.println(future.isDone());
+			while (true) {
+				
+				if(future.isDone())
+				{ArrayList<databaen> x = future.get();
+				for (int i = 0; i < x.size(); i++) {
+					
+					databaen databaen = x.get(i);
+					System.out.println(databaen.toString());
+
+				}
+				x.clear();
+				break;
+					
+				}
+				
+			}
+		}
+
+	}
+
+}
+
+
